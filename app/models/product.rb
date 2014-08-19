1b939b0263c5fddf01a8448e9397dea26d4f00af
Jk <jkwynk@gmail.com>
2014-08-19 15:58:10 -0400

    added formatted price

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,10 @@ class Product < ActiveRecord::Base
 
 	validates :description, :name, presence: true
 	validates :price_in_cents, numericality: {only_integer: true, greater_than: 0}
+
+
+	def formatted_price
+		price_in_dollars = price_in_cents.to_f / 100
+		sprintf("%.2f", price_in_dollars)
+	end
 end
