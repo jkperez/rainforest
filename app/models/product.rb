fb1551a3b586f66f189e209b695ecdc4ea24bbc6
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-08-20 10:03:57 -0400

    fixed error when price_in_cents is greater than sql's intmax

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
 
 	validates :description, :name, presence: true
-	validates :price_in_cents, numericality: {only_integer: true, greater_than: 0}
+	validates :price_in_cents, numericality: { only_integer: true, greater_than: 0 }
+	validates :price_in_cents, numericality: { less_than: (2**31)-1, message: "is too large."}
 
 	has_many :reviews
 
