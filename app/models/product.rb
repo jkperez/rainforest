4ea0d0aefb678c942d8690f6da4d76b9707793b8
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-08-20 14:17:47 -0400

    fixed price_in_cents display

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,18 @@ class Product < ActiveRecord::Base
 
 	def formatted_price
 		price_in_dollars = price_in_cents.to_f / 100
-		sprintf("%.2f", price_in_dollars)
+
+		price_array = sprintf("%.2f", price_in_dollars).split('.')
+		price_dollars = price_array[0]
+		price_cents = price_array[1]
+
+		place = price_dollars.length - 3
+
+		while place >= 2
+			price_dollars.insert(place, ',')
+			place -= 3
+		end
+
+		"$" + price_dollars + "." + price_cents;
 	end
 end
