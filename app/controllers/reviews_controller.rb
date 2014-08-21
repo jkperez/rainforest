17fba55f3b2e4bc5f119607bd5fe814d68e94e56
Jk <jkwynk@gmail.com>
2014-08-21 13:12:40 -0400

    implemented ratings on reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -73,6 +73,6 @@ class ReviewsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
-      params.require(:review).permit(:comment, :product_id)
+      params.require(:review).permit(:comment, :product_id, :rating)
     end
 end
