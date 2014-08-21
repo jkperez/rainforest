a207084f567e0d1a1fc06d6d7d38fa7dff0bddd1
Jk <jkwynk@gmail.com>
2014-08-21 09:52:34 -0400

    ratings added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821031645) do
+ActiveRecord::Schema.define(version: 20140821134941) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140821031645) do
     t.integer  "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "rating"
   end
 
   add_index "reviews", ["product_id"], name: "index_reviews_on_product_id"
