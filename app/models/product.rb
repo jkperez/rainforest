90ad823380e0696ba39fa9342d9708be1ca52bf1
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-08-22 15:00:12 -0400

    implemented sale price display

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,10 +10,9 @@ class Product < ActiveRecord::Base
 
 	has_many :reviews
 
-	def formatted_price
-		price_in_dollars = price_in_cents.to_f / 100
 
-		price_in_dollars = sprintf("%.2f", price_in_dollars)
+	def stringify_cost(cost)
+		price_in_dollars = sprintf("%.2f", cost)
 
 		place = price_in_dollars.rindex('.') - 3
 
@@ -22,7 +21,27 @@ class Product < ActiveRecord::Base
 			place -= 3
 		end
 
-		"<span class='price'>$#{price_in_dollars}</span>".html_safe
+		price_in_dollars
+	end
+
+	def formatted_price
+		result = "".html_safe
+
+		price_in_dollars = price_in_cents.to_f / 100
+
+		if sale && sale < 100 
+			result = "<span class='regular-price'>$#{stringify_cost(price_in_dollars)}</span><br>".html_safe
+			price_in_dollars *= sale
+		end
+
+		result += "<span class='price'>$#{stringify_cost(price_in_dollars)}</span>".html_safe
+	end
+
+	def formatted_sale
+		# sales are only on if there's a discount
+		if(sale && sale < 100)
+			"SALE! #{100 - sale}% off!"
+		end
 	end
 
 	def large_thumbnail 
