08b5deefa9e67d7d871b7fb4127ecd8d9360d3fd
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-08-24 14:41:22 -0400

    added check for adding items to a cart that already has them

--- app/controllers/cart_item_controller.rb
+++ app/controllers/cart_item_controller.rb
@@ -2,10 +2,18 @@ class CartItemController < ApplicationController
 	before_action :set_cart_item, only: [:update]
 
 	def create 
-		cart_item = CartItem.new
-		cart_item.cart = Cart.take
-		cart_item.product = Product.find params[:p]
-		cart_item.quantity = 1;
+		cart = Cart.take
+		product_id = params[:p]
+
+		cart_item = CartItem.find_by(cart_id: cart.id, product_id: product_id)
+		if(cart_item)
+			cart_item.quantity += 1;
+		else
+			cart_item = CartItem.new
+			cart_item.cart = cart
+			cart_item.product = Product.find product_id
+			cart_item.quantity = 1;
+		end
 
 		respond_to do |format|
 			if(cart_item.save)
